b8a97cd2cb39005c0106862aa010d4aa2d20f127
Marina-Romany <marinaromany41@gmail.com>
2026-06-28 10:24:44 +0300

    fix vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/maromart-ecommerce/',
-
   plugins: [
     react(),
     tailwindcss(),
